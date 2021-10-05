14fb8f13eeaa695150b0dc54c0aadc65ce3947bc
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-10-05 15:55:27 -0500

    adds initial postgreSQL setup

--- src/main/java/com/hkimbrough22/songr/SongrApplication.java
+++ src/main/java/com/hkimbrough22/songr/SongrApplication.java
@@ -2,8 +2,9 @@ package com.hkimbrough22.songr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SongrApplication {
 
 	public static void main(String[] args) {
